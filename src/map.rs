ef2e9aef800e63c7ff32d173c2fb1cbd0ad809be
weldsk <>
2019-11-03 21:20:11 +0900

    DirectionにJumpの追加

--- src/map.rs
+++ src/map.rs
@@ -17,6 +17,8 @@ pub enum Direction
     LowerLeft,
     /// 右下
     LowerRight,
+    /// 瞬間移動(方向性を持たない)
+    Jump,
 }
 
 impl Direction
@@ -41,20 +43,35 @@ impl Direction
     {
         Vec::new() as Vec<Direction>
     }
+
+    pub fn invert(&self) -> Self
+    {
+        match self
+        {
+            Direction::Up           => Direction::Down,
+            Direction::Down         => Direction::Up,
+            Direction::Left         => Direction::Right,
+            Direction::Right        => Direction::Left,
+            Direction::UpperLeft    => Direction::LowerRight,
+            Direction::UpperRight   => Direction::LowerLeft,
+            Direction::LowerLeft    => Direction::UpperRight,
+            Direction::LowerRight   => Direction::UpperLeft,
+            Direction::Jump         => Direction::Jump,
+        }
+    }
 }
 
-pub fn in_event_default(current_tile:Tile, next_tile:Tile, to_dir: Direction)
+pub fn in_event_default(current_tile:Tile, next_tile:Tile, from_dir: Direction)
 {
 }
 
+#[allow(unused_variables)]
 pub fn on_event_default(current_tile:Tile)
 {
-    &current_tile;
 }
 
 pub fn out_event_default(current_tile:Tile, next_tile:Tile, to_dir: Direction)
 {
-
 }
 
 /// 地形(allowの項目に一つでも合致していれば侵入可能)
