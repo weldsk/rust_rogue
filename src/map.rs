fc5d559d0a61ede4a3eaface82f27727d6b69900
weldsk <w3ldsk@gmail.com>
2019-11-03 17:39:42 +0900

    map.rs DirectionとTerrainTypeの追加

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,25 @@
-
-struct Map
+pub enum Direction
 {
-    
+    Up,
+    Down,
+    Right,
+    Left,
+    UpperLeft,
+    UpperRight,
+    LowerLeft,
+    LowerRight,
 }
+
+
+pub struct TerrainType
+{
+    allow_from_dir: Vec<Direction>,
+    allow_to_dir: Vec<Direction>,
+}
+
+pub struct Terrain
+{
+    terrain_type: TerrainType,
+}
+
+pub struct Map (Vec<Vec::<Terrain>>);
