2c3d693c3f3015a83a1dfee5fd783e090db82222
weldsk <>
2019-11-06 02:26:02 +0900

    remove struct Core

--- src/core.rs
+++ src/core.rs
@@ -4,26 +4,3 @@ pub mod entity;
 pub mod common;
 pub mod terrain;
 pub mod database;
-use database::*;
-use std::sync::Arc;
-use std::sync::Mutex;
-use lazy_static::lazy_static;
-
-lazy_static! {
-//static mut SYSTEM: &'static Option<Rc<Core>> = &None;
-    pub static ref SYSTEM: Arc<Mutex<Core>> = Arc::new(Mutex::new(Core::new()));
-}
-
-pub struct Core {
-    pub terrain_db: database::Database<terrain::Terrain>,
-}
-
-impl Core {
-    pub fn new() -> Self
-    {
-        Core
-        {
-            terrain_db: Database::new(),
-        }
-    }
-}
