649a7932ebdf950fbc35f0141e8125f5e7c0477d
weldsk <w3ldsk@gmail.com>
2019-11-09 23:14:46 +0900

    move map_gen.rs -> map.rs add config.rs add pub function in game.rs

--- src/game.rs
+++ src/game.rs
@@ -1,1 +1,16 @@
-pub mod map_gen;
+mod map;
+mod config;
+use crate::*;
+
+/// データベースへ読み込み
+pub fn database(database: &mut core::database::Database) -> &mut core::database::Database
+{
+    // ロード
+    return database;
+}
+
+/// マップ生成
+pub fn generate_map(level: i32) -> core::map::Map
+{
+    map::generate(config::MAP_SIZE_X, config::MAP_SIZE_Y)
+}
---
+++ src/game/config.rs
@@ -0,0 +1,2 @@
+pub const MAP_SIZE_X: usize = 80;
+pub const MAP_SIZE_Y: usize = 40;
